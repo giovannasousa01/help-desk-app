3b83b1cfb08dff766d2e25e6ea89656eb77d063f
Giovanna Sousa <giovannasousa.code@gmail.com>
2023-02-22 15:12:33 -0400

    Primeiro Commit / First Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HelpDeskApp());
+}
+
+class HelpDeskApp extends StatelessWidget {
+  const HelpDeskApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'HelpDesk App',
+      
+    );
+  }
+}
