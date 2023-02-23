268c15938ad4eb000c88ab1ffc5c82f0ef676eb5
Giovanna Sousa <giovannasousa.code@gmail.com>
2023-02-23 16:07:49 -0400

    Criação do AppColors da aplicação

---
+++ lib/Utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color(0xff3c74a6);
+  static Color textColorBlue = const Color(0xff2d80c9);
+  static Color textColorRed = const Color(0xffa63c3c);
+  static Color buttonRed = const Color(0xffa63c3c);
+  static Color backgroundCard = const Color(0xfff0f2f5);
+  static Color textDescriptionCard = const Color(0xff969cb2);
+  static Color primaryOpacityColor = const Color.fromRGBO(60, 116, 166, 0.09);
+}
