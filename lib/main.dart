56108a6ede911e7962addc5530a0b0494c4d6e57
Giovanna Sousa <giovannasousa.code@gmail.com>
2023-02-27 16:34:56 -0400

    Adicionando a dependencia lottie e configurando o theme do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: unused_local_variable
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:help_desk_app/Screens/register/register_screen.dart';
+import 'package:help_desk_app/utils/app_colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  //db.collection("usuarios").doc("001").set({"nome": "Giovanna Sousa Jimenez"});
+
   runApp(const HelpDeskApp());
 }
 
@@ -9,9 +21,13 @@ class HelpDeskApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'HelpDesk App',
-      
+      theme: ThemeData(
+        primaryColor: AppColors.textColorBlue,
+      ),
+      home: const RegisterScreen(),
     );
   }
 }
